07941eb193e22131af174b6f32b8e3cf9a025383
Syd <srlg99g@gmail.com>
2024-10-13 00:14:28 -0400

    add users index and pagination

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def avatar_for(user, options = { size: 80})
+    email_address = user.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    size = options[:size]
+    avatar_url = "https://robohash.org/#{hash}.png?bgset=any&size=#{size}x#{size}"
+
+    image_tag(avatar_url, alt: user.username, class: "rounded-circle shadow mx-auto d-block")
+  end
 end
