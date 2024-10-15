b59dc1325e78064cb4b06d2a4aa789a2a25a1b90
Syd <srlg99g@gmail.com>
2024-10-15 00:37:06 -0400

    tab rename, back button on articles, copyright opens new tab, temporary avatar added for robohash disconnections

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,14 @@
 module ApplicationHelper
-  def avatar_for(user, options = { size: 80})
+  def avatar_for(user, options = { size: 80 })
     email_address = user.email.downcase
     hash = Digest::MD5.hexdigest(email_address)
     size = options[:size]
     avatar_url = "https://robohash.org/#{hash}.png?bgset=any&size=#{size}x#{size}"
 
-    image_tag(avatar_url, alt: user.username, class: "rounded-circle shadow mx-auto d-block")
+    fallback_avatar_url = asset_path('1000_F_444656182_ByXJUFSBtKVUwWOv1ZyZm2rms7bGVP9i.jpg')
+
+    image_tag(avatar_url, alt: user.username, class: "rounded-circle shadow mx-auto d-block",
+              onerror: "this.onerror=null; this.src='#{fallback_avatar_url}'; this.width=#{size};")
   end
 
 end
