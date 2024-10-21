69d5694464a15b829ec3dd86e2f8a7eb97889317
Syd <srlg99g@gmail.com>
2024-10-20 22:01:59 -0400

    Add searchbar placeholder Users/Articles, Replaced interpolation code for search controller, stylized search index, login added to home

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,8 +4,8 @@ class SearchController < ApplicationController
     @users = []
 
     if params[:query].present?
-      @articles = Article.where("title LIKE ? OR description LIKE ?", "%#{params[:query]}%", "%#{params[:query]}%")
-      @users = User.where("username LIKE ? OR email LIKE ?", "%#{params[:query]}%", "%#{params[:query]}%")
+      @articles = Article.where("title LIKE '%#{params[:query]}%' OR description LIKE '%#{params[:query]}%'")
+      @users = User.where("username LIKE '%#{params[:query]}%' OR email LIKE '%#{params[:query]}%'")
     end
   end
 end
